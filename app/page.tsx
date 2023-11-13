cda6475b02298770fc3fc6903062c76188902ef7
AhianyoElisha <elisha.ahianyo247@gmail.com>
2023-11-13 19:57:10 +0000

    started working on Samsoft landing page

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Truck from "@/components/Truck";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Truck />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  )
+}
