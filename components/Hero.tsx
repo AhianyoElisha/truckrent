3b4f2931629c80a1a823d215d7607a75a6bfbdba
AhianyoElisha <elisha.ahianyo247@gmail.com>
2023-11-30 21:11:15 +0000

    created the guide, features, getApp, truck sections of the landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Button from './Button'
 const Hero = () => {
     return (
         <section className='max-container padding-container
-        flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+        flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
             <div className='hero-map' />
                 {/* LEFT */}
                 <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
@@ -55,6 +55,33 @@ const Hero = () => {
                         icon='/play.svg'
                         variant='btn_white_text'
                     />
+                </div>
+            </div>
+            <div className='relative flex flex-1 items-start'>
+                <div className='relative z-20 w-[268px] flex flex-col gap-8
+                rounded-3xl bg-green-90 px-7 py-8'>
+                    <div className='flex flex-col'>
+                        <div className='flexBetween'>
+                            <p className='regular-16 text-gray-20'>Location</p>
+                            <Image
+                                src="/close.svg"
+                                alt='close'
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                        <p className='bold-20 text-white'>Kumasi - Accra</p>
+                    </div>
+                    <div className='flexBetween'>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 text-gray-20'>Distance</p>
+                            <p className='bold-20 text-white'>200km</p>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 text-gray-20'>Delivery Time</p>
+                            <p className='bold-20 text-white'>7 hours</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
